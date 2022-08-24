c027087ae12ed01901e591b23f780d0f98dd6806
Danilo Queiroz <39010493+daniloqueirooz@users.noreply.github.com>
2022-08-24 15:53:56 -0300

    Exercicio Lógico

---
+++ Par&Impar.kt
@@ -0,0 +1,29 @@
+package com.example.studykotlin
+
+import java.util.*
+
+fun main() {
+    val reader = Scanner(System.`in`)
+    val I = reader.nextInt()
+
+    if (I % 2 == 0) {
+        print("PAR")
+    } else {
+        print("IMPAR")
+    }
+
+}
+/* var max: Int
+
+if (a > b) {
+    max = a
+} else {
+    max = b
+}
+ */
+
+
+//BREAK POINT (PONTO DE QUEBRA)
+
+
+
