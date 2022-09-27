eecdebe826b73bfac973b3b7fb18deffc2384814
Danilo Queiroz <39010493+daniloqueirooz@users.noreply.github.com>
2022-09-27 20:25:02 -0300

    Mutaveis<Int>
    Listas mutaveis

---
+++ Mutaveis.kt
@@ -0,0 +1,18 @@
+package com.example.listakotlin
+
+fun main() {
+    val numbers = listOf(1, 2, 3, 4, 5, 6)
+    println("List: $numbers")
+    println("Size: ${numbers.size}")
+
+    println("First element: ${numbers[0]}")
+    println("Second element: ${numbers[1]}")
+    println("Last index: ${numbers.size - 1}")
+    println("Last element: ${numbers[numbers.size - 1]}")
+    println("First: ${numbers.first()}")
+    println("Last: ${numbers.last()}")
+
+    
+    println("Contains 4? ${numbers.contains(4)}")
+    println("Contains 7? ${numbers.contains(7)}")
+}
