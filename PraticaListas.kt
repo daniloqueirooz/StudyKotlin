b8fc9b7cd4d82dfe809702b9946acf1be5da9982
Danilo Queiroz <39010493+daniloqueirooz@users.noreply.github.com>
2022-10-01 10:12:54 -0300

    Pedidos

---
+++ PraticaListas.kt
@@ -0,0 +1,30 @@
+package com.example.listakotlin
+
+open class Item(val name: String, val price: Int)
+
+class Noodles : Item("Noodles", 10) {
+    override fun toString(): String {
+        return name
+    }
+}
+
+
+class Vegetables(vararg val toppings: String) : Item("Vegetables", 5) {
+    override fun toString(): String {
+        if (toppings.isEmpty()) {
+            return "$name Chef's Choice"
+
+        } else {
+            return name + " " + toppings.joinToString()
+        }
+    }
+}
+
+fun main() {
+    val noodles = Noodles()
+    val vegetables = Vegetables(": Repolho", "Brotos", "Cebola")
+    val vegetables2 = Vegetables()
+    println(noodles)
+    println(vegetables)
+    print(vegetables2)
+}
