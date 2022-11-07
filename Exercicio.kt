6e2a6b597f343d584a02eaeb4496ca6589996d24
Danilo Queiroz <39010493+daniloqueirooz@users.noreply.github.com>
2022-11-07 16:33:47 -0300

    Add files via upload
    Exercício

---
+++ Exercicio.kt
@@ -0,0 +1,11 @@
+package com.example.appfast
+
+fun main () {
+
+    val set1: Set<String> = setOf<String>("Paris", "Barcela", "Madrid", "China")
+    val set2: MutableSet<String> = mutableSetOf<String>()
+
+    print(set1.size)
+   
+
+}
